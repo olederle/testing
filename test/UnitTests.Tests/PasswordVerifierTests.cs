7328d05dfc52ae45c6fb51b7896c8d3b5cf08342
olederle <dev@lederle24.at>
2025-03-07 00:19:57 +0100

    PasswordVerifier tests

--- test/UnitTests.Tests/PasswordVerifierTests.cs
+++ test/UnitTests.Tests/PasswordVerifierTests.cs
@@ -1,3 +1,41 @@
 namespace UnitTests.Tests;
 
-public class PasswordVerifierTests { }
+public class PasswordVerifierTests
+{
+    [
+        Theory,
+        InlineData("aB7a", false), // invalid class
+        InlineData("aB7abcdefgh", true), // valid class
+        InlineData("aB7jklu", false), // upper boundary invalid
+        InlineData("aB7jklua", true) // lower boundary valid
+    ]
+    public void ValidatePasswordByLength(string password, bool isValid)
+    {
+        // act
+        bool isValidPassword = PasswordVerifier.Validate(password);
+
+        // assert
+        Assert.Equal(isValid, isValidPassword);
+    }
+
+    [Fact]
+    public void ValidatePasswortWithoutLowerLetter()
+    {
+        bool isValidPassword = PasswordVerifier.Validate("123456789A");
+        Assert.False(isValidPassword);
+    }
+
+    [Fact]
+    public void ValidatePasswortWithoutUperLetter()
+    {
+        bool isValidPassword = PasswordVerifier.Validate("123456789a");
+        Assert.False(isValidPassword);
+    }
+
+    [Fact]
+    public void ValidatePasswortWithoutNumber()
+    {
+        bool isValidPassword = PasswordVerifier.Validate("abcdefghijkLM");
+        Assert.False(isValidPassword);
+    }
+}
