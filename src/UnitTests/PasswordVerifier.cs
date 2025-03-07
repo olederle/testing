054058005ada7196543314ff38545206a3ea246c
olederle <dev@lederle24.at>
2025-03-07 09:22:52 +0100

    update PasswordVerifier

--- src/UnitTests/PasswordVerifier.cs
+++ src/UnitTests/PasswordVerifier.cs
@@ -1,5 +1,54 @@
 namespace UnitTests;
 
+public enum PasswordVerificationRule
+{
+    NotNull,
+    Length,
+    Lower,
+    Upper,
+    Digit,
+}
+
+public struct PasswordVerificationResult
+{
+    public bool IsValid { get; }
+    public PasswordVerificationRule Rule { get; }
+    public string Message { get; }
+
+    public PasswordVerificationResult(PasswordVerificationRule rule, string message)
+    {
+        IsValid = false;
+        Rule = rule;
+        Message = message;
+    }
+
+    public PasswordVerificationResult(PasswordVerificationRule rule)
+    {
+        IsValid = true;
+        Rule = rule;
+        Message = string.Empty;
+    }
+
+    public void ThrowIfInvalid()
+    {
+        if (IsValid == false)
+        {
+            throw new PasswordVerificationException([this]);
+        }
+    }
+}
+
+public class PasswordVerificationException : Exception
+{
+    public List<PasswordVerificationResult> VerificationResults { get; }
+
+    public PasswordVerificationException(List<PasswordVerificationResult> verificationResults)
+        : base()
+    {
+        VerificationResults = verificationResults;
+    }
+}
+
 public class PasswordVerifier
 {
     //    OK -> 1. - password should be larger than 8 chars
@@ -8,23 +57,64 @@ public class PasswordVerifier
     //    4. - password should have one lowercase letter at least
     //    5. - password should have one number at least
 
-    public static bool Validate(string password)
+    public static void Validate(string password)
     {
         if (password == null)
-            return false;
+        {
+            new PasswordVerificationResult(
+                PasswordVerificationRule.NotNull,
+                "password should not be null"
+            ).ThrowIfInvalid();
+            return;
+        }
+
+        List<PasswordVerificationResult> invalids = [];
 
         if (password.Length < 8)
-            return false;
+        {
+            invalids.Add(
+                new PasswordVerificationResult(
+                    PasswordVerificationRule.Length,
+                    "password should be larger than 8 chars"
+                )
+            );
+        }
 
         if (!password.Any(Char.IsLower))
-            return false;
+        {
+            invalids.Add(
+                new PasswordVerificationResult(
+                    PasswordVerificationRule.Lower,
+                    "password should have one lowercase letter at least"
+                )
+            );
+        }
 
         if (!password.Any(Char.IsUpper))
-            return false;
+        {
+            invalids.Add(
+                new PasswordVerificationResult(
+                    PasswordVerificationRule.Upper,
+                    "password should have one uppercase letter at least"
+                )
+            );
+        }
 
         if (!password.Any(Char.IsDigit))
-            return false;
+        {
+            invalids.Add(
+                new PasswordVerificationResult(
+                    PasswordVerificationRule.Digit,
+                    "password should have one number at least"
+                )
+            );
+        }
 
-        return true;
+        if (invalids.Count > 1)
+        {
+            throw new PasswordVerificationException(invalids);
+        }
+
+        return;
     }
 }
--- test/UnitTests.Tests/PasswordVerifierTests.cs
+++ test/UnitTests.Tests/PasswordVerifierTests.cs
@@ -4,38 +4,68 @@ public class PasswordVerifierTests
 {
     [
         Theory,
-        InlineData("aB7a", false), // invalid class
-        InlineData("aB7abcdefgh", true), // valid class
-        InlineData("aB7jklu", false), // upper boundary invalid
-        InlineData("aB7jklua", true) // lower boundary valid
+        InlineData("aB7abcdefgh"), // valid class
+        InlineData("aB7jklua") // lower boundary valid
     ]
-    public void ValidatePasswordByLength(string password, bool isValid)
+    public void ValidatePasswordByLength(string password)
     {
         // act
-        bool isValidPassword = PasswordVerifier.Validate(password);
+        PasswordVerifier.Validate(password);
+    }
 
-        // assert
-        Assert.Equal(isValid, isValidPassword);
+    [
+        Theory,
+        InlineData("aBaa"), // invalid class
+        InlineData("aBajklu"), // upper boundary invalid
+    ]
+    public void DoesThrowForInvalidLength(string password)
+    {
+        PasswordVerificationException ex = Assert.Throws<PasswordVerificationException>(() =>
+        {
+            PasswordVerifier.Validate(password);
+        });
+
+        Assert.Equal(2, ex.VerificationResults.Count);
+        Assert.Contains(
+            PasswordVerificationRule.Length,
+            ex.VerificationResults.Select(v => v.Rule)
+        );
+        Assert.Contains(PasswordVerificationRule.Digit, ex.VerificationResults.Select(v => v.Rule));
     }
 
     [Fact]
     public void ValidatePasswortWithoutLowerLetter()
     {
-        bool isValidPassword = PasswordVerifier.Validate("123456789A");
-        Assert.False(isValidPassword);
+        PasswordVerificationException ex = Assert.Throws<PasswordVerificationException>(() =>
+        {
+            PasswordVerifier.Validate("123456789A");
+        });
+
+        Assert.False(ex.VerificationResult.IsValid);
+        Assert.Equal(PasswordVerificationRule.Lower, ex.VerificationResult.Rule);
     }
 
     [Fact]
     public void ValidatePasswortWithoutUperLetter()
     {
-        bool isValidPassword = PasswordVerifier.Validate("123456789a");
-        Assert.False(isValidPassword);
+        PasswordVerificationException ex = Assert.Throws<PasswordVerificationException>(() =>
+        {
+            PasswordVerifier.Validate("123456789a");
+        });
+
+        Assert.False(ex.VerificationResult.IsValid);
+        Assert.Equal(PasswordVerificationRule.Upper, ex.VerificationResult.Rule);
     }
 
     [Fact]
     public void ValidatePasswortWithoutNumber()
     {
-        bool isValidPassword = PasswordVerifier.Validate("abcdefghijkLM");
-        Assert.False(isValidPassword);
+        PasswordVerificationException ex = Assert.Throws<PasswordVerificationException>(() =>
+        {
+            PasswordVerifier.Validate("abcdefghijkLM");
+        });
+
+        Assert.False(ex.VerificationResult.IsValid);
+        Assert.Equal(PasswordVerificationRule.Digit, ex.VerificationResult.Rule);
     }
 }
